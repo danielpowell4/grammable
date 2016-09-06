a0ef5581459eac6e64af6b1a0c6c128c5b51ff4d
Dan <danielpowell12@msn.com>
2016-09-06 12:50:34 -0600

    Add grams#create controller action

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -7,4 +7,16 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 end
